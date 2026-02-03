63cd18b83b7b43811c9f4ab032177fd353d80ca3
faizzaidi23 <786faizzaidi@gmail.com>
2026-02-03 22:55:30 +0530

    updated the AppNavGraph too

--- app/src/main/java/com/example/shopshere/UserInterface/navigation/1_AppNavgraph.kt
+++ app/src/main/java/com/example/shopshere/UserInterface/navigation/1_AppNavgraph.kt
@@ -3,6 +3,7 @@ package com.example.shopshere.UserInterface.navigation
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -10,15 +11,23 @@ import androidx.navigation.compose.rememberNavController
 import com.example.shopshere.UserInterface.Components.BottomBar
 import com.example.shopshere.UserInterface.Screens.CartScreen
 import com.example.shopshere.UserInterface.Screens.HomeScreen
+import com.example.shopshere.UserInterface.Screens.LoginScreen
 import com.example.shopshere.UserInterface.Screens.OrderScreen
 import com.example.shopshere.UserInterface.Screens.ProductDetailScreen
 import com.example.shopshere.UserInterface.Screens.ProfileScreen
+import com.example.shopshere.UserInterface.Screens.RegisterScreen
 import com.example.shopshere.UserInterface.Screens.SearchScreen
 import com.example.shopshere.UserInterface.Screens.WishlistScreen
+import com.example.shopshere.data.repository.AuthRepository
 
 @Composable
 fun AppNavGraph(){
     val navController= rememberNavController()
+
+    val repo=remember{ AuthRepository() }
+
+    val startDestination=if(repo.currentUser!=null)"home"
+    else "login"
 
     Scaffold(
         bottomBar = {
@@ -27,7 +36,7 @@ fun AppNavGraph(){
     ) { paddingValues ->
         NavHost(
             navController = navController,
-            startDestination = "home",
+            startDestination = startDestination,
             modifier = Modifier.padding(paddingValues)
         ){
             composable(BottomNavItem.Home.route){
@@ -46,7 +55,7 @@ fun AppNavGraph(){
                 BottomNavItem.Profile.route
             ){
                 ProfileScreen(
-                    onOrderClick = {navController.navigate("order")},
+//                    onOrderClick = {navController.navigate("order")},
                     onLogoutClick = {}
                 )
             }
@@ -59,6 +68,28 @@ fun AppNavGraph(){
 
             composable("order"){
                 OrderScreen()
+            }
+
+            composable("login"){
+                LoginScreen(
+                    onLoginSuccess = {
+                        navController.navigate("home"){
+                            popUpTo("login"){inclusive=true}
+                        }
+                    },
+                    onNavigateToRegister = {
+                        navController.navigate("register")
+                    }
+                )
+            }
+
+
+            composable("register screen"){
+                RegisterScreen(
+                    onBackToLogin = {
+                        navController.popBackStack()
+                    }
+                )
             }
         }
     }
